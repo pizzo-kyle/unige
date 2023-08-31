fa192acbe9269d1dbb517669c464612e2093df32
Necroxid <128234128+Necroxid@users.noreply.github.com>
2023-08-31 18:55:05 +0200

    Add files via upload

--- SQL/progetto_trigger.sql
+++ SQL/progetto_trigger.sql
@@ -1,49 +1,34 @@
 set search_path to 'Progetto_BD2023';
 
-CREATE FUNCTION ContaRepliche() RETURNS trigger AS
+CREATE OR REPLACE FUNCTION ContaSpecie() RETURNS trigger AS
 $$
+DECLARE TempScuola VARCHAR(30);
 BEGIN
-IF ((SELECT COUNT(SpeciePianta)
-FROM REPLICA R
-JOIN CLASSE C ON C.codc=R.classedimora
-JOIN SCUOLA S ON S.codmec=C.scuola
-GROUP BY S.codmec
-HAVING COUNT(SpeciePianta) >3
-)>3)
-THEN
---RAISE EXCEPTION 'internal error';
-RETURN NULL;
-ELSE
-RETURN NEW;
-END IF;
+    IF ((SELECT COUNT (DISTINCT SpeciePianta)
+    FROM REPLICA R
+    JOIN Orto O ON O.CodOrto=R.Orto
+    JOIN Scuola S ON O.Scuola=S.CodMec
+    WHERE NEW.SpeciePianta <> R.SpeciePianta AND (SELECT S1.CodMec
+                                                    FROM Orto O1 
+                                                  JOIN Scuola S1 ON S1.CodMec = O1.Scuola
+                                                  WHERE NEW.Orto = O1.CodOrto) = S.CodMec 
+    GROUP BY S.CodMec
+    HAVING COUNT(SpeciePianta) >=3
+    )>=3)
+    THEN
+    SELECT S.Nome INTO TempScuola
+        FROM Scuola S JOIN Orto ON Scuola = CodMec
+        WHERE CodOrto = NEW.Orto;
+        RAISE EXCEPTION 'La Scuola % sta già monitorando 3 specie', TempScuola;
+    RETURN NULL;
+    ELSE
+    RETURN NEW;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
-
 
 
 CREATE TRIGGER SPECIE3
-BEFORE INSERT ON REPLICA
-EXECUTE PROCEDURE ContaRepliche();
-
-
-
-
-CREATE FUNCTION ContaReplik() RETURNS trigger AS
-$$
-BEGIN
-IF 'true'
-THEN
---RAISE EXCEPTION 'internal error';
-RETURN NULL;
-ELSE
-RETURN NEW;
-END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
-
-CREATE TRIGGER SPEKIE
-BEFORE INSERT ON REPLICA
-EXECUTE PROCEDURE ContaReplik();
+BEFORE INSERT OR UPDATE ON REPLICA
+FOR EACH ROW EXECUTE PROCEDURE ContaSpecie();
 
