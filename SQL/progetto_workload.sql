973d9ba3be6ff8173d3a639436c8a7c172af6b0d
Massi1399 <116018459+Massi1399@users.noreply.github.com>
2023-09-03 12:58:52 +0200

    Add files via upload

--- SQL/progetto_workload.sql
+++ SQL/progetto_workload.sql
@@ -1,21 +1,20 @@
 set search_path to 'Progetto_BD2023';
 
-
 --W1
 --Determinare il numero totale di piante per ogni orto pulito
 SELECT O.CodOrto, COUNT(R.CodRepl)
 FROM Replica R 
   JOIN Orto O ON R.Orto = O.CodOrto
-GROUP BY O.CodOrto
-WHERE O.Pulito = TRUE;
+WHERE O.Pulito = TRUE  
+GROUP BY O.CodOrto;
 
 
 --W2
---Determinare le repliche messe a dimora in data odierna
+--Determinare le repliche messe a dimora in vaso in data odierna
 SELECT R.CodRepl, O.CodOrto
 FROM Replica R 
   JOIN Orto O ON R.Orto = O.CodOrto
-WHERE DataDimora = CURRENT_DATE;
+WHERE DataDimora = CURRENT_DATE AND Tipo = 'Vaso';
 
 
 --W3
