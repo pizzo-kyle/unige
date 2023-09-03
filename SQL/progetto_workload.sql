ecf36b151e69f5a89f755237f047a0585539686d
Massi1399 <116018459+Massi1399@users.noreply.github.com>
2023-09-03 11:52:13 +0200

    Update progetto_workload.sql

--- SQL/progetto_workload.sql
+++ SQL/progetto_workload.sql
@@ -2,25 +2,25 @@ set search_path to 'Progetto_BD2023';
 
 
 --W1
---Determinare il numero totale di piante per ogni orto
-SELECT O.CodOrto, COUNT(CodRepl)
-FROM Replica R JOIN Orto O ON R.Orto = O.CodOrto
+--Determinare il numero totale di piante per ogni orto pulito
+SELECT O.CodOrto, COUNT(R.CodRepl)
+FROM Replica R 
+  JOIN Orto O ON R.Orto = O.CodOrto
 GROUP BY O.CodOrto
-;
+WHERE O.Pulito = TRUE;
 
 
 --W2
 --Determinare le repliche messe a dimora in data odierna
 SELECT R.CodRepl, O.CodOrto
-FROM Replica R
-JOIN Orto O ON R.Orto = O.CodOrto
+FROM Replica R 
+  JOIN Orto O ON R.Orto = O.CodOrto
 WHERE DataDimora = CURRENT_DATE;
-;
 
 
 --W3
---Per ogni rilevazione, determinare l'entità che l'ha effettuata
+--Per ogni rilevazione antecedente alla data odierna, determinare da chi è stata effettuata effettuata
 SELECT Ril.CodRil, R.CodResp, R.Tipo
-FROM Rilevazione Ril
-JOIN Responsabile R ON Ril.RespRil = R.CodResp
-;
+FROM Rilevazione Ril 
+  JOIN Responsabile R ON Ril.RespRil = R.CodResp
+WHERE DataRil < CURRENT_DATE;
